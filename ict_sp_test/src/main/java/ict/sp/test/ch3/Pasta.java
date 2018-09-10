08c7bb63f26b628d3b204ec2aaaa5a68e4a0f6bb
koitt03-A <koitt03-A@koitt03-A-PC>
2018-09-10 17:29:02 +0900

    ffg

--- ict_sp_test/src/main/java/ict/sp/test/ch3/Pasta.java
+++ ict_sp_test/src/main/java/ict/sp/test/ch3/Pasta.java
@@ -1,8 +1,10 @@
 package ict.sp.test.ch3;
 
+import org.springframework.core.annotation.Order;
 import org.springframework.stereotype.Service;
 
 @Service
+@Order(3)
 public class Pasta implements Food{
 
 	@Override
--- ict_sp_test/src/main/java/ict/sp/test/ch3/Pizza.java
+++ ict_sp_test/src/main/java/ict/sp/test/ch3/Pizza.java
@@ -1,8 +1,10 @@
 package ict.sp.test.ch3;
 
+import org.springframework.core.annotation.Order;
 import org.springframework.stereotype.Service;
 
 @Service
+@Order(2)
 public class Pizza implements Food{
 
 	@Override
